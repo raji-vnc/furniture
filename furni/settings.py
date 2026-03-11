c3f658dbc110573c0ffd440f9f5cec7b2f73c9ef
Raji VN <vnraji46@gmail.com>
2026-03-11 12:38:12 +0530

    new

--- furni/settings.py
+++ furni/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'furni.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -79,8 +79,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'furni.wsgi.application'
 
 CORS_ALLOW_ALL_ORIGINS = True
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -93,8 +91,6 @@ DATABASES = {
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -112,8 +108,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/6.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -124,8 +118,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
 SIMPLE_JWT = {
