4149fd61e9f3ecd1fdd49770ed123a0d5e2fa15d
Raji VN <vnraji46@gmail.com>
2026-03-11 12:59:20 +0530

    new

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,10 @@
 const API_URL='http://127.0.0.1.8000/api/products'
+
+fetch(API_URL)
+.then(res=>res.json())
+.then(products=>{
+    let container=document.getElementById('container')
+    products.slice(0,3).forEach(element => {
+       container.innerHTML+=`` 
+    });
+})
