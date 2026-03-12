c6ecec633856bfcdfa88167bf03b7f638dd9c115
Raji VN <vnraji46@gmail.com>
2026-03-12 12:44:08 +0530

    new

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,12 @@
 from django import views
 from django.urls import path
-from products.views import index
+from products.views import index,products,cart,checkout,payment
 
+app_name='products'
 urlpatterns=[
-    path('',index,name='index')
+    path('',index,name='index'),
+    path('products/',products,name='products'),
+    path('cart/',cart,name='cart'),
+    path('checkout/',checkout,name='checkout'),
+    path('payment/',payment,name='payment')
 ]
--- products/views.py
+++ products/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
+
+def products(request):
+    return render(request,'shop.html')
+def cart(request):
+    return render(request,'cart.html')
+def checkout(request):
+    return render(request,'checkout.html')
+def payment(request):
+    return render(request,'payment_success.html')
+
+
+
