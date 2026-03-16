5ade0630364c69d17653e6acc95deeea1812793b
Raji VN <vnraji46@gmail.com>
2026-03-16 15:45:39 +0530

    new

--- api/cart/urls.py
+++ api/cart/urls.py
@@ -1,18 +1,12 @@
 from django import views
 from rest_framework.routers import DefaultRouter
 from django.urls import path
-from .views import add_to_cart,view_cart,remove_cart_item,update_cart,increase_cart_quantity,decrese_cart_quantity
+from .views import add_to_cart
 from .views import CartItemViewSet
 router = DefaultRouter()
-router.register(r'cart-items', CartItemViewSet, basename='cartitem')
+router.register(r'cartitems', CartItemViewSet, basename='cartitem')
 urlpatterns = router.urls
 
 urlpatterns=[
-    path('add/',add_to_cart),
-    path('view/',view_cart),
-    path('remove/',remove_cart_item),
-    path('update/',update_cart),
-    path('cart/increase/<int:id>/',increase_cart_quantity),
-    path('cart/decrease/<int:id>/',decrese_cart_quantity),
-
+    path('add/',add_to_cart)
 ]
--- api/cart/views.py
+++ api/cart/views.py
@@ -1,125 +1,43 @@
 from rest_framework.viewsets import ModelViewSet
 from .serializers import CartItemSerializer
 from cart.models import CartItem,Cart
-from rest_framework.decorators import api_view 
+from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
 from products.models import Product
+from django.shortcuts import get_object_or_404
+from rest_framework.permissions import AllowAny,IsAuthenticated
+from rest_framework import status
 
 class CartItemViewSet(ModelViewSet):
     queryset = CartItem.objects.all()
     serializer_class = CartItemSerializer
-
+    permission_classes = [IsAuthenticated]
 
 @api_view(['POST'])
 def add_to_cart(request):
-    user=request.user
-    product_id=request.data.get("product_id")
-    quantity=request.data.get("quantity",1)
-    product=Product.objects.get(id=product_id)
-    cart,created=Cart.objects.get_or_create(user=user)
-    cartitem,created=CartItem.objects.get_or_create(
-        cart=cart,
+    user = request.user
+    product_id = request.data.get('product_id')
+    quantity = request.data.get('quantity', 1)
+
+    try:
+        product = Product.objects.get(id=product_id)
+    except Product.DoesNotExist:
+        return Response({"error": "Product not found"}, status=404)
+
+    cart_item, created = Cart.objects.get_or_create(
+        user=user,
         product=product
     )
 
     if not created:
-        cartitem.quantity+=int(quantity)
-        cart.save()
+        cart_item.quantity += int(quantity)
+    else:
+        cart_item.quantity = quantity
 
-    return Response({"message":"Product added"})
-
-@api_view(['GET'])
-def view_cart(request):
-    user=request.user
-    try:
-        cart=Cart.objects.get(user=user)
-    except Cart.DoesNotExist:
-        return Response({"message":"Cart is empty"})
-    
-
-    cartitems=CartItem.objects.filter(cart=cart)
-    data=[]
-    total_price=0
-    for item in cartitems:
-        item_total=item.product.price * item.quantity
-        total_price +=item_total
-
-        data.append({
-            "product_id":item.product.id,
-            "product_name":item.product.name,
-            "price":item.product.price,
-            "quantity":item.quantity,
-            "total":item_total
-        })
-        return Response({
-            "cart_items":data,
-            "cart_total":total_price
-        })
-
-@api_view(['DELETE'])
-def remove_cart_item(request):
-    cart_item_id=request.data.get("cart_item_id")
-    try:
-        cartitem=CartItem.objects.get(id=cart_item_id)
-    except CartItem.DoesNotExist:
-        return Response({"error":"Cart item not found"})
-    cartitem.delete()
-    return Response({"message":"cart item removed"})
-
-@api_view(['PUT'])
-def update_cart(request):
-    cart_item_id=request.data.get("cart_item_id")
-    quantity=request.data.get('quantity')
-
-    try:
-        cart_item=CartItem.objects.get(id=cart_item_id)
-
-    except CartItem.DoesNotExist:
-        return Response({"error":"cart item not found"})
-    if int(quantity) <=0:
-        cart_item.delete()
-        return Response({"message":"item removed from cart"})
-    cart_item.quantity=quantity
     cart_item.save()
 
     return Response({
-        "message":"cart updated successfully",
-        "product":cart_item.product.name,
-        "quantity":cart_item.quantity
-    })
-
-@api_view(['POST'])
-def increase_cart_quantity(request,id):
-    try:
-        cart=CartItem.objects.get(id=id)
-        cart.quantity+=1
-        cart.save()
-        return Response({
-            "message":"Cart quantity increased",
-            "quantity":cart.quantity
-        })
-        
-    except CartItem.DoesNotExist:
-        return Response({"error":"Cart is not found"})
-    
-@api_view(['POST'])
-def decrese_cart_quantity(request,id):
-    try:
-        cart=CartItem.objects.get(id=id)
-        if cart.quantity >1:
-            cart.quantity-=1
-            cart.save()
-        else:
-            cart.delete()
-            return Response({
-                "message":"Product removed"
-            })
-        return Response({
-            "message":"Cart quantity decresed",
-            "quantity":cart.quantity
-        })
-    except CartItem.DoesNotExist:
-        return Response({
-            "error":"cart not found"
-        })
-
+        "message": "Product added to cart",
+        "product": product.name,
+        "quantity": cart_item.quantity
+    }, status=status.HTTP_200_OK)
