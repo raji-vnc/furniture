d1243182d59a6214bece589cc6365b80885172ac
Raji VN <vnraji46@gmail.com>
2026-06-02 10:58:56 +0530

    Added new

--- furni/settings.py
+++ furni/settings.py
@@ -7,11 +7,11 @@ import os
 from dotenv import load_dotenv
 
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3'
-    )
-}
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default='sqlite:///db.sqlite3'
+#     )
+# }
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / ".env")
